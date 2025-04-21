07930880c54414121b8b903373c8ba940b58276a
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-04-21 07:12:13 -0400

    feat: adding the throwable for second teleport mec
    - right now you can throw an object then teleport to that obj

--- script/player.gd
+++ script/player.gd
@@ -6,12 +6,19 @@ const JUMP_VELOCITY = -500.0
 const FALLING_IMPULSE = -10.0
 const REWIND_SIZE = 5
 var previous_positions: RingQueue
+
 @export var marker: PackedScene
+@export var throwable: PackedScene
+@export var THROWABLE_IMPULSE = 1000
+var throwable_information: RingQueue
+@export var MAX_THROWABLE = 1
 
 func _init() -> void:
 	previous_positions = RingQueue.new()
 	previous_positions.init(REWIND_SIZE)
-
+	throwable_information = RingQueue.new()
+	throwable_information.init(MAX_THROWABLE)
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -26,6 +33,21 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("rewind"):
 		var new_position = previous_positions.pop()
 		if new_position != null:
+			position = new_position
+
+	if Input.is_action_just_pressed("throw"):
+		var item = throwable.instantiate()
+		item.position = $ThrowableLocation.global_position
+		var direction = Vector2.RIGHT
+		item.linear_velocity = direction * THROWABLE_IMPULSE
+		throwable_information.add_item(item)
+		get_parent().add_child(item)
+
+	if Input.is_action_just_pressed("teleport"):
+		var throwable_item = throwable_information.pop()
+		if throwable_item != null:
+			var new_position = throwable_item.global_position
+			new_position.y -= 30
 			position = new_position
 
 	# Get the input direction and handle the movement/deceleration.
@@ -43,5 +65,3 @@ func _on_rewind_capture_timer_timeout() -> void:
 	var mark = marker.instantiate()
 	mark.position = position
 	get_parent().add_child(mark)
-	print("Adding: %s" % position)
-	print("New state of queue: %s" % previous_positions)
