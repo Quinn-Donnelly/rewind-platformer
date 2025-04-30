f02a431c1138450081e52976c52d5aabb8f10195
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-04-30 06:51:02 -0400

    feat: fall fast trivially implemented, coupled fashion
    - I have states tied together if they trigger and action of the other state,
      for instance Idle and Fall are coupled for jump, Fine for now
    will be ignored, and an empty message aborts the commit. # # On branch trunk
    script/movement-state-machine/fall-state.gd #	modified:
    script/movement-state-machine/idle-state.gd # # ------------------------ >8
    ------------------------ # Do not modify or remove the line above. # Everything
    below it will be ignored. diff --git
    a/script/movement-state-machine/fall-state.gd
    b/script/movement-state-machine/fall-state.gd index ce1b3f1..5424195 100644 ---
    a/script/movement-state-machine/fall-state.gd +++
    b/script/movement-state-machine/fall-state.gd @@ -2,6 +2,9 @@ extends State
    class_name Fall
    
     var bufferedJump: bool = true
    +var initial_height: float
    +const JUMP_VELOCITY = -500.0
    +const MAX_JUMP_HEIGHT: float = 130.0
    
     func enter(previous_state: State) -> void:
     	bufferedJump = false
    @@ -9,6 +12,11 @@ func enter(previous_state: State) -> void:
     func _physics_process(delta: float) -> void:
     	if $"../..".is_on_floor():
     		state_machine.change_state("Idle")
    +		return
    +
    +	var remaining_height = $"../..".position.y -  (initial_height - MAX_JUMP_HEIGHT)
    +	if remaining_height <= 0:
    +		$"../..".velocity.y *= -2.5
    
     func handle_input(event: InputEvent) -> void:
     	if event.is_action_pressed("jump"):
    @@ -17,3 +25,7 @@ func handle_input(event: InputEvent) -> void:
    
     func _on_fall_timer_timeout() -> void:
     	bufferedJump = false
    +
    +func jump() -> void:
    +	initial_height = $"../..".position.y
    +	$"../..".velocity.y = JUMP_VELOCITY
    diff --git a/script/movement-state-machine/idle-state.gd b/script/movement-state-machine/idle-state.gd
    index 4a2df46..83fdfce 100644
    --- a/script/movement-state-machine/idle-state.gd
    +++ b/script/movement-state-machine/idle-state.gd
    @@ -1,11 +1,9 @@
     extends State
     class_name Idle
    
    -const JUMP_VELOCITY = -500.0
    -
     func enter(previous_state: State) -> void:
     	if previous_state is Fall and previous_state.bufferedJump:
    -		$"../..".velocity.y = JUMP_VELOCITY
    +		state_machine.states["Fall"].jump()
     		state_machine.change_state("Fall")
    
     # Called when the node enters the scene tree for the first time.
    @@ -18,5 +16,5 @@ func _physics_process(delta: float) -> void:
     		state_machine.change_state("Fall")
    
     	if Input.is_action_just_pressed("jump") and $"../..".is_on_floor():
    -		$"../..".velocity.y = JUMP_VELOCITY
    +		state_machine.states["Fall"].jump()
     		state_machine.change_state("Fall")

--- script/movement-state-machine/fall-state.gd
+++ script/movement-state-machine/fall-state.gd
@@ -2,6 +2,9 @@ extends State
 class_name Fall
 
 var bufferedJump: bool = true
+var initial_height: float
+const JUMP_VELOCITY = -500.0
+const MAX_JUMP_HEIGHT: float = 130.0
 
 func enter(previous_state: State) -> void:
 	bufferedJump = false
@@ -9,6 +12,11 @@ func enter(previous_state: State) -> void:
 func _physics_process(delta: float) -> void:
 	if $"../..".is_on_floor():
 		state_machine.change_state("Idle")
+		return
+	
+	var remaining_height = $"../..".position.y -  (initial_height - MAX_JUMP_HEIGHT)
+	if remaining_height <= 0:
+		$"../..".velocity.y *= -2.5
 
 func handle_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
@@ -17,3 +25,7 @@ func handle_input(event: InputEvent) -> void:
 
 func _on_fall_timer_timeout() -> void:
 	bufferedJump = false
+
+func jump() -> void:
+	initial_height = $"../..".position.y
+	$"../..".velocity.y = JUMP_VELOCITY
--- script/movement-state-machine/idle-state.gd
+++ script/movement-state-machine/idle-state.gd
@@ -1,11 +1,9 @@
 extends State
 class_name Idle
 
-const JUMP_VELOCITY = -500.0
-
 func enter(previous_state: State) -> void:
 	if previous_state is Fall and previous_state.bufferedJump:
-		$"../..".velocity.y = JUMP_VELOCITY
+		state_machine.states["Fall"].jump()
 		state_machine.change_state("Fall")
 
 # Called when the node enters the scene tree for the first time.
@@ -18,5 +16,5 @@ func _physics_process(delta: float) -> void:
 		state_machine.change_state("Fall")
 	
 	if Input.is_action_just_pressed("jump") and $"../..".is_on_floor():
-		$"../..".velocity.y = JUMP_VELOCITY
+		state_machine.states["Fall"].jump()
 		state_machine.change_state("Fall")
