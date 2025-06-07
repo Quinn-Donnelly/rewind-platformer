ba22689e879b0031abe0804ce96ff48431af367e
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-06-07 13:36:55 -0400

    feat: inital time based score
    - needs to be refactored to the stage model

--- script/GameManager.gd
+++ script/GameManager.gd
@@ -4,18 +4,30 @@ signal win
 
 var score: int = 0 
 var is_game_over: bool = false
+var stage_time: int = 0
 
 func _ready() -> void:
-	score = 0
 	$Camera/UI/RestartLabel.visible = false
 	$Camera/UI/GameOverLabel.visible = false
+	start_game()
+
+func start_game() -> void:
+	stage_time = Time.get_ticks_msec()
 	is_game_over = false
+	score = 0
 
 func game_over() -> void:
 	is_game_over = true
 	$Camera/UI/GameOverLabel.visible = true
 
 func win_game() -> void:
+	# TODO: refactor stage_time name
+	var duration = Time.get_ticks_msec() - stage_time
+	var expected_time = 10000.0
+	var stage_time_expected_points = 100.0
+	var time_score = stage_time_expected_points * (expected_time / float(duration))
+	addScore(time_score)
+	
 	win.emit()
 	is_game_over = true
 	$Player.set_process(false)
